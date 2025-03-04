32c087505046543bfe9902fde84bfe1c9707595f
Happy <104614758+happyprajapati@users.noreply.github.com>
2025-03-04 16:56:32 +0530

    add fetch gloally

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const fetch = require ("node-fetch")
+globalThis.fetch = fetch;
 
 const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require("@google/generative-ai");
 
