f4e24716e3b2b44c8613dca55f11c079405d14dc
Happy <104614758+happyprajapati@users.noreply.github.com>
2025-03-04 16:50:12 +0530

    update the sever.js code

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const fetch = require ("node-fetch")
 
 const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require("@google/generative-ai");
 
